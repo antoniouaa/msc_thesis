be9e8d9ca772d2b04a3e718e9aa458bb39aab265
antoniouaa <antoniouaa@hotmail.com>
2020-06-10 19:08:51 +0100

    scrapes data from tiingo for some tickers, src now made available

---
+++ src/discriminator.py
@@ -0,0 +1,32 @@
+import torch.nn as nn
+
+class Discriminator(nn.Module):
+    def __init__(self, opt):
+        super(Discriminator, self).__init__()
+
+        def discriminator_block(in_filters, out_filters, bn=True):
+            block = [nn.Conv2d(in_filters, out_filters, 3, 2, 1),
+                nn.LeakyReLU(0.2, inplace=True),
+                nn.Dropout2d(0.25)
+            ]
+            if bn:
+                block.append(nn.BatchNorm2d(out_filters, 0.8))
+            return block
+
+        self.mode1 = nn.Sequential(
+            *discriminator_block(opt.channels, 16, bn=False),
+            *discriminator_block(16, 32),
+            *discriminator_block(32, 64),
+            *discriminator_block(64,128)
+        )
+
+        ds_size = opt.img_size // 2 ** 4
+        self.adv_layer = nn.Sequential(nn.Linear(128 * ds_size ** 2, 1), nn.Sigmoid())
+
+    def forward(self, x):
+        out = self.model(img)
+        out = out.view(out.shape[0], -1)
+        validity = self.adv_layer(out)
+        return validity
+
+    
---
+++ src/scraper.py
@@ -0,0 +1,50 @@
+import os
+import requests
+import csv
+
+BASE_PATH = os.path.dirname(__file__)
+
+def fetch_credentials():
+    FILENAME = "API_KEY.txt"
+    FILEPATH = os.path.abspath(os.path.join(BASE_PATH, "..", FILENAME))
+
+    credentials = {}
+    with open(FILEPATH) as f:
+        for line in f:
+            words = line.split("=")
+            credentials[words[0]] = words[1]
+        return credentials
+
+def read_tickers(filename="supported_tickers.csv"):
+    FILEPATH = os.path.abspath(os.path.join(BASE_PATH, "../data/tickers", filename))
+    with open(FILEPATH) as f:
+        tickers = [row.split(",")[0] for row in f]
+        return tickers[1:]
+
+def make_request(ticker, start_date=None, end_date=None):
+    URL = f"https://api.tiingo.com/tiingo/daily/{ticker}/prices?startDate={start_date}&endDate={end_date}&token={credentials['TIINGO']}"
+    print(URL)
+    req = requests.get(URL)
+    req_json = req.json()
+    return req_json
+
+def save_to_csv(data, filename="ticker_data.csv", headers=[]):
+    FILEPATH = os.path.abspath(os.path.join(BASE_PATH, "../data/tickers/ticker_data", filename))
+    with open(FILEPATH, "w", newline="") as datacsv:
+        csvwriter = csv.writer(datacsv, delimiter=",")
+        csvwriter.writerow(headers)
+        for line in data:
+            csvwriter.writerow(line.values())
+
+if __name__ == "__main__":
+    credentials = fetch_credentials()
+    tickers = read_tickers()
+    for ticker in tickers[0:len(tickers):200]:
+        try:
+            data = make_request(ticker, start_date="2018-01-01", end_date="2018-12-01")
+            #print(data)
+            if data:
+                headers = data[0].keys()
+                save_to_csv(data, filename=f"{ticker}.csv", headers=headers)
+        except:
+            pass
