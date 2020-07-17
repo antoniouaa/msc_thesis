66f0afe986aa358f8ccb1ebc247d176ac733d5d1
antoniouaa <antoniouaa@hotmail.com>
2020-07-17 23:56:11 +0100

    improved lstm.ipynb with a working model, updated READme to reflect current status of the project

--- README.md
+++ README.md
@@ -1,5 +1,52 @@
-MSc Comp Finance repo
+# MSc Computational Finance module CF981: CCFEA MSc Dissertation
 
-nothing to see here
+_A work-in-progress_
 
-move along
+
+## Forecasting daily Closing Prices of Equity symbols using ANNs
+
+The project implements a Recurrent Neural Network (RNN) in the form of two Long Short-Term Memory (LSTM) layers to attempt to predict future closing prices for stocks.
+
+### Data
+Data is pulled via the [tiingo API](https://api.tiingo.com/), which includes daily values for open, high, low, close and trading volume of a wide variety of symbols. We chose symbols listed under the New York Stock Exchange.
+
+### Methodology, so far...
+Predictions are made using raw data, even though we computed other composite metrics like Stochastic Oscillator indices, weighted averages, Bollinger bands. More work is needed to incorporate those metrics in the network which will hopefully result in more accurate predictions. The 3 features in the data are `open`, `high` and `low` values which are used to attempt to predict how the `close` price will look like at the end of the trading day. The data aren't scaled, though future work will probably involve a scaling transformation.
+
+Our model currently is a sequential single layer LSTM RNN + Dense, though future work will add one more LSTM layer. 100 LSTM nodes are currently used. [Rectified Linear Units (ReLU)](https://keras.io/api/layers/activations/) are used as activation functions and [Adam](https://arxiv.org/abs/1412.6980) as the optimiser.
+
+```
+model = Sequential()
+model.add(LSTM(100, activation="relu", input_shape=(15, 3)))
+model.add(Dense(1))
+opt = Adam(learning_rate=0.01)
+model.compile(loss="mean_squared_error", optimizer=opt)
+```
+
+### Results, so far...
+
+![Predictions on the testing set of a stock dataset](AIZP.png)
+Trying to fit the model onto the testing data seems to work, not as accurate as I would like it to be.
+Reported loss and accuracy: 
+```
+History Item [loss] reports value of 1.8945788939793904
+History Item [accuracy] reports value of 0.5555555820465088
+History Item [mae] reports value of 1.0775936841964722
+```
+
+Accuracy over 50% is ok but could be better. Will tinker with learning rate, loss function, activation function, node count, etc.
+
+![AIZP symbol training and testing set over actual](assets/MA_AIZP.jpg)
+
+### Tech used
+The usual python data stack is used in this project:
+* pandas
+* numpy
+* sklearn
+* statsmodels
+
+For deep learning we are using:
+* Keras
+* TensorFlow
+
+
--- src/lstm/lstm.py
+++ src/lstm/lstm.py
@@ -1,16 +1,13 @@
 import os
 import numpy as np 
 import pandas as pd 
-import matplotlib.pyplot as plt 
-import torch
-import torch.nn as nn
-from torch.autograd import Variable
-from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
 import keras
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
-from keras.layers import GRU
 from keras.optimizers import Adam
 
 TICKER_DIR = "C:\\Users\\anton\\Documents\\antoniouaa\\msc_thesis\\data\\tickers\\ticker_data\\_Rolling"
@@ -26,50 +23,27 @@ def sliding_windows(data, seq_length):
         _y = data[i+seq_length]
         x.append(_x)
         y.append(_y)
-    return np.array(x), np.array(y)    
+    return np.array(x), np.array(y)
 
-def train_test_split(data, SC, seq_length=2, size=0.8):
-    training_data = SC.fit_transform(training)
-    x, y = sliding_windows(training_data, seq_length)
-    train_size = int(len(y) * size)
-    test_size = len(y) - train_size
-    dataX = Variable(torch.Tensor(x))
-    dataY = Variable(torch.Tensor(y))
-    trainX = Variable(torch.Tensor(x[:train_size]))
-    trainY = Variable(torch.Tensor(y[:train_size]))
-    testX = Variable(torch.Tensor(x[train_size-1:]))
-    testY = Variable(torch.Tensor(y[train_size-1:]))
-    return dataX, dataY, trainX, trainY, testX, testY
-
-def build_model(type_, loss="mean_squared_error", lr=0.01, batch=2, timesteps=2, features=2):
+def build_model(loss="mean_squared_error", lr=0.01, batch=2, timesteps=2, features=2):
     m = Sequential()
-    if type_.lower() == "lstm":
-        m.add(LSTM(features, batch_input_shape=(batch, timesteps, features), stateful=True, return_sequences=True))
-        m.add(LSTM(features, batch_input_shape=(batch, timesteps, features), stateful=True))
-        m.add(Dense(features))
-    if type_.lower() == "gru":
-        m.add(GRU(features, batch_input_shape=(batch, timesteps, features), return_sequences=True))
-        m.add(Dense(features))
+    m.add(LSTM(features, batch_input_shape=(batch, timesteps, features), stateful=True, return_sequences=True))
+    m.add(LSTM(features, batch_input_shape=(batch, timesteps, features), stateful=True))
+    m.add(Dense(features))
     opt = Adam(learning_rate=lr)
-    m.compile(loss="mean_squared_error", optimizer=opt)
+    m.compile(loss="mean_squared_error", optimizer=opt, metrics=["accuracy"])
     return m
 
 def fit_model(model, trainX, trainY, num_epochs=2000, batch_size=2):
-    for i in range(10):
-        try:
-            model.fit(trainX, trainY, epochs=num_epochs, batch_size=batch_size, verbose=2, shuffle=False)
-            model.reset_states()
-        except:
-            pass
+    model.fit(trainX, trainY, epochs=num_epochs, batch_size=batch_size, verbose=2, shuffle=False)
     return model
 
 def model_predict(model, trainX, testX, trainY, testY, SC=MinMaxScaler, batch=2):
     train_predict = model.predict(trainX, batch_size=batch)
-    model.reset_states()
     test_predict = model.predict(testX, batch_size=batch)
     train_predict = SC.inverse_transform(train_predict)
-    trainY = SC.inverse_transform(trainY)
     test_predict = SC.inverse_transform(test_predict)
+    trainY = SC.inverse_transform(trainY)
     testY = SC.inverse_transform(testY)
     return train_predict, trainY, test_predict, testY
 
@@ -90,25 +64,31 @@ def graph(training_data, train_predict, test_predict, ticker_name, SC, open_vals
     fig.suptitle("Training and Testing datasets ontop of Actual Opening Prices")
     plt.ylabel("Opening Price ($)")
     plt.xlabel("Trading Days")
-    fig.savefig(f"C:\\Users\\anton\\Documents\\antoniouaa\\msc_thesis\\assets\\{ticker_name}.jpg")
+    # fig.savefig(f"C:\\Users\\anton\\Documents\\antoniouaa\\msc_thesis\\assets\\{ticker_name}.jpg")
     plt.close()
-    
-tickers = fetch_tickers(TICKER_DIR)
 
-for tick in tickers:
-    ticker_name = tick[:-4]
-    batch_size, timesteps, features = 2, 2, 2
-    num_epochs, learning_rate = 1, 0.01
-    train_size = 0.8
 
-    training = pd.read_csv(f"{TICKER_DIR}\\{tick}", header=0)
-    training = training.tail(160).iloc[:, 0:4:2].values
-    open_vals = training[:, -1]
+if __name__ == "__main__":
+    tickers = fetch_tickers(TICKER_DIR)
 
-    scaler = MinMaxScaler()
-    dataX, dataY, trainX, trainY, testX, testY = train_test_split(training, SC=scaler)
-    model = build_model("gru", lr=learning_rate, batch=batch_size, timesteps=timesteps, features=features)
-    model = fit_model(model, trainX, trainY, num_epochs=num_epochs, batch_size=batch_size)
-    train_predict, trainY, test_predict, testY = model_predict(model, trainX, testX, trainY, testY, SC=scaler, batch=batch_size)
-    graph(training, train_predict, test_predict, ticker_name, scaler, open_vals)
-    keras.backend.clear_session()
+    for tick in tickers:
+        ticker_name = tick.split(".")[0]
+        batch_size, timesteps, features = 1, 10, 10
+        num_epochs, learning_rate = 1, 0.01
+        train_size = 0.8
+
+        dataset = pd.read_csv(f"{TICKER_DIR}\\{tick}", header=0)
+        X = dataset.tail(160).values[:, 1:]
+        y = dataset.tail(160).values[:, 0]
+
+        scaler = MinMaxScaler()
+        X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8)
+        model = build_model("gru", lr=learning_rate, batch=batch_size, timesteps=timesteps, features=features)
+        print(X_train.shape)
+        print(y_train.shape)
+        X_train = X_train.reshape(1, 10, X_train.shape[1])
+        y_train = y_train.reshape(y_train.shape[0], 1)
+        model.fit(X_train, y_train, epochs=num_epochs, batch_size=batch_size, verbose=2, shuffle=False)
+        train_predict, y_train, test_predict, y_test = model_predict(model, X_train, X_test, y_train, y_test, SC=scaler, batch=batch_size)
+        graph(training, train_predict, test_predict, ticker_name, scaler, open_vals)
+        keras.backend.clear_session()
