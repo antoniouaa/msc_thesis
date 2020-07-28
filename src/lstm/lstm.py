2e41b5ce41710eaf59601f110f0efeac4c49d9f2
antoniouaa <antoniouaa@hotmail.com>
2020-07-28 21:32:24 +0100

    apply model to all datasets and generate prediction graphs

--- src/lstm/lstm.py
+++ src/lstm/lstm.py
@@ -1,94 +1,93 @@
 import os
 import numpy as np 
-import pandas as pd 
+import pandas as pd
 import matplotlib.pyplot as plt
 import keras
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
+from keras.layers import Dense, LSTM
 from keras.optimizers import Adam
 
-TICKER_DIR = "C:\\Users\\anton\\Documents\\antoniouaa\\msc_thesis\\data\\tickers\\ticker_data\\_Rolling"
+TICKER_DIR = "C:\\Users\\anton\\Documents\\antoniouaa\\msc_thesis\\data\\tickers\\ticker_data"
 ASSET_DIR = "C:\\Users\\anton\\Documents\\antoniouaa\\msc_thesis\\assets"
 
 def fetch_tickers(path):
-    return [tick for tick in os.listdir(path)]
+    tickers = [tick for tick in os.listdir(os.path.abspath(TICKER_DIR)) if os.path.isfile(os.path.join(TICKER_DIR, tick))]
+    return tickers
 
-def sliding_windows(data, seq_length):
-    x, y = [], []
-    for i in range(len(data)-seq_length-1):
-        _x = data[i:(i+seq_length)]
-        _y = data[i+seq_length]
-        x.append(_x)
-        y.append(_y)
-    return np.array(x), np.array(y)
+def split_sequences(sequences, n_steps):
+	X, y = [], []
+	for i, _ in enumerate(sequences):
+		end_ix = i + n_steps
+		if end_ix > len(sequences):
+			break
+		seq_x, seq_y = sequences[i:end_ix, :], sequences[end_ix-1, :]
+		X.append(seq_x)
+		y.append(seq_y)
+	return np.array(X), np.array(y)
 
-def build_model(loss="mean_squared_error", lr=0.01, batch=2, timesteps=2, features=2):
+def build_model(loss="mae", lr=0.01, n_steps=5, n_features=4):
     m = Sequential()
-    m.add(LSTM(features, batch_input_shape=(batch, timesteps, features), stateful=True, return_sequences=True))
-    m.add(LSTM(features, batch_input_shape=(batch, timesteps, features), stateful=True))
-    m.add(Dense(features))
+    m.add(LSTM(100, activation="relu", return_sequences=True, input_shape=(n_steps, n_features)))
+    m.add(LSTM(n_features, activation="relu"))
+    m.add(Dense(n_features))
     opt = Adam(learning_rate=lr)
-    m.compile(loss="mean_squared_error", optimizer=opt, metrics=["accuracy"])
+    m.compile(loss=loss, optimizer=opt, metrics=["mae", "mse"])
     return m
 
-def fit_model(model, trainX, trainY, num_epochs=2000, batch_size=2):
-    model.fit(trainX, trainY, epochs=num_epochs, batch_size=batch_size, verbose=2, shuffle=False)
-    return model
-
-def model_predict(model, trainX, testX, trainY, testY, SC=MinMaxScaler, batch=2):
-    train_predict = model.predict(trainX, batch_size=batch)
-    test_predict = model.predict(testX, batch_size=batch)
-    train_predict = SC.inverse_transform(train_predict)
-    test_predict = SC.inverse_transform(test_predict)
-    trainY = SC.inverse_transform(trainY)
-    testY = SC.inverse_transform(testY)
-    return train_predict, trainY, test_predict, testY
-
-def graph(training_data, train_predict, test_predict, ticker_name, SC, open_vals):
-    offset = len(test_predict)
-    train_predict_plot = np.empty_like(training_data[:, -1])
-    train_predict_plot[:] = np.nan
-    train_predict_plot[:len(train_predict)] = train_predict[:, -1]
-
-    test_predict_plot = np.empty_like(training_data[:, -1])
-    test_predict_plot[:] = np.nan
-    test_predict_plot[len(train_predict)+1:len(training_data)-3] = test_predict[:, -1]
-
-    fig = plt.figure()
-    plt.plot(open_vals)
-    plt.plot(train_predict_plot[:])
-    plt.plot(test_predict_plot[:])
-    fig.suptitle("Training and Testing datasets ontop of Actual Opening Prices")
-    plt.ylabel("Opening Price ($)")
-    plt.xlabel("Trading Days")
-    # fig.savefig(f"C:\\Users\\anton\\Documents\\antoniouaa\\msc_thesis\\assets\\{ticker_name}.jpg")
-    plt.close()
-
+def evaluate_model(model, X_test, y_test):
+    scores = model.evaluate(X_test, y_test, verbose=0)
+    print(*[f"History Item [{item}] reports value of {value}" for value, item in zip(scores, model.metrics_names)], sep="\n")
+    return scores
 
 if __name__ == "__main__":
+    np.random.seed(1)
     tickers = fetch_tickers(TICKER_DIR)
+    dfs = [pd.read_csv(os.path.join(TICKER_DIR, tick)) for tick in tickers]
+    
+    num_epochs, learning_rate = 100, 0.01
+    n_steps = 3
 
-    for tick in tickers:
-        ticker_name = tick.split(".")[0]
-        batch_size, timesteps, features = 1, 10, 10
-        num_epochs, learning_rate = 1, 0.01
-        train_size = 0.8
-
-        dataset = pd.read_csv(f"{TICKER_DIR}\\{tick}", header=0)
-        X = dataset.tail(160).values[:, 1:]
-        y = dataset.tail(160).values[:, 0]
-
-        scaler = MinMaxScaler()
-        X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8)
-        model = build_model("gru", lr=learning_rate, batch=batch_size, timesteps=timesteps, features=features)
-        print(X_train.shape)
-        print(y_train.shape)
-        X_train = X_train.reshape(1, 10, X_train.shape[1])
-        y_train = y_train.reshape(y_train.shape[0], 1)
-        model.fit(X_train, y_train, epochs=num_epochs, batch_size=batch_size, verbose=2, shuffle=False)
-        train_predict, y_train, test_predict, y_test = model_predict(model, X_train, X_test, y_train, y_test, SC=scaler, batch=batch_size)
-        graph(training, train_predict, test_predict, ticker_name, scaler, open_vals)
+    for tick, df in zip(tickers, dfs):
         keras.backend.clear_session()
+
+        ticker_name = tick.split(".")[0]
+
+        assert df.shape == (1006, 13)
+
+        target = df["close"].tail(1000)
+        dataset = df[["high", "low", "open"]].tail(1000)
+        high = dataset["high"].values.reshape(len(dataset), 1)
+        low = dataset["low"].values.reshape(len(dataset), 1)
+        open_ = dataset["open"].values.reshape(len(dataset), 1)
+        close = target.values.reshape(len(dataset), 1)
+        d = np.hstack((open_, high, low, close))
+
+        X, y = split_sequences(d, n_steps)
+        assert X.shape == (998, 3, 4)
+        assert y.shape == (998, 4)
+        n_features = X.shape[2]
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, shuffle=False)
+
+        model = build_model(n_steps=n_steps, n_features=n_features)
+        model.fit(X_train, y_train, epochs=num_epochs, verbose=0)
+
+        print(f"Evaluating Ticker {ticker_name}")
+        scores = evaluate_model(model, X_test, y_test)
+
+        actual = df["close"].tail(X_test.shape[0]).values
+        yhat = model.predict(X_test, verbose=0)
+        predictions = yhat[:, -1]
+        fig = plt.figure()
+        plt.title("Actual v Predicted prices")
+        plt.ylabel("Closing Price")
+        plt.xlabel("Trading Day")
+        plt.plot(actual, label="actual")
+        plt.plot(predictions, label="predictions")
+        plt.grid()
+        plt.legend()
+        plt.savefig(os.path.join(ASSET_DIR, f"{ticker_name}.png"))
+
+        del model
